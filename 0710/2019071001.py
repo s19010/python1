5063b60b7341342efba29f8510b063fd281e6beb
Eiki Tamashiro <s19010@std.it-college.ac.jp>
2019-07-10 17:19:55 +0900

    課題0710 独学プログラマ p69 の課題

---
+++ 0710/2019071001.py
@@ -0,0 +1,6 @@
+def square():
+    num = int(input("数字を入力してください"))
+    return num ** num
+
+
+print(square())
---
+++ 0710/2019071003.py
@@ -0,0 +1,6 @@
+def f(x, y, z, a=0, b=1):
+    return x + y + z + a + b
+
+
+print(f())
+print(f(1, 2, 3))
